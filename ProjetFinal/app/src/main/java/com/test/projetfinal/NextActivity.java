cf4d2181b8b2520f3c6b02c30da6c415206577d9
Matthieu <matthieu.veron@minesdedouai.fr>
2017-04-01 03:21:20 +0200

    Création Next-Activity

---
+++ ProjetFinal/app/src/main/java/com/test/projetfinal/NextActivity.java
@@ -0,0 +1,16 @@
+package com.test.projetfinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bromy on 01/04/2017.
+ */
+
+public class NextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
